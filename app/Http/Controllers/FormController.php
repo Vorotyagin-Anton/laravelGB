a48959edb92665d494ff96a0ad6d8f425686b72d
ANTON VOROTYAGIN <57726031+Vorotyagin-Anton@users.noreply.github.com>
2021-01-17 23:40:31 +0300

    hw04
    add new form

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormController extends Controller
+{
+    public function getDataForm(Request $request)
+    {
+        if ($request->isMethod('get')) {
+            return view('getDataForm')
+                ->with('categories', $this->getCategories());
+        }
+
+        if ($request->isMethod('post')) {
+            file_put_contents('test.txt', json_encode($request->all()), FILE_APPEND);
+            return redirect()->route('getDataForm');
+        }
+    }
+}
