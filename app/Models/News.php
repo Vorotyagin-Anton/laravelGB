db0b462f4c7b70023800a46167d3272b92dc2b79
ANTON VOROTYAGIN <57726031+Vorotyagin-Anton@users.noreply.github.com>
2021-03-20 17:24:57 +0300

    hw07
    add validation for add news form

--- app/Models/News.php
+++ app/Models/News.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
+
+    public static function getValidationRules(): array
+    {
+        return [
+            'title' => 'required|max:255',
+            'inform' => 'required'
+        ];
+    }
+
+    public static function getAttributesNames(): array
+    {
+        return [
+            'title' => 'Заголовок',
+            'inform' => 'Содержание новости'
+        ];
+    }
 }
